88b6337acae570033fde50027b7e5b0eb42742ad
Tommy Pranoto <tommyprn8@gmail.com>
2020-12-08 15:56:09 +0700

    selesai flow backend untuk fitur 1

--- models/orderreg-model.js
+++ models/orderreg-model.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const orderregSchema = new Schema({
-	invoiceId: { type: String, required: true },
-	companyId: { type: mongoose.Types.ObjectId, required: true, ref: 'Company' },
-	packageName: { type: String, required: true },
-	status: { type: String, required: true, default: 'Pending' },
-	createdAt: { type: Date, required: true },
-	approvedtAt: { type: Date, default: null },
-	dueDate: { type: Date, required: true },
-	slot: { type: Number, required: true },
-	pricePerSlot: { type: Number, required: true },
-	totalPrice: { type: Number, required: true }
+  invoiceId: { type: String, required: true },
+  companyId: { type: mongoose.Types.ObjectId, required: true, ref: "Company" },
+  packageName: { type: String, required: true },
+  status: { type: String, required: true, default: "Pending" },
+  createdAt: { type: Date, required: true },
+  approvedAt: { type: Date, default: null },
+  dueDate: { type: Date, required: true },
+  slot: { type: Number, required: true },
+  pricePerSlot: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Orderreg', orderregSchema);
+module.exports = mongoose.model("Orderreg", orderregSchema);
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const { check } = require('express-validator');
-const adminControllers = require('../controllers/admin-controllers');
+const express = require("express");
+const { check } = require("express-validator");
+const adminControllers = require("../controllers/admin-controllers");
 
 const router = express.Router();
-const checkAuth = require('../middleware/check-auth');
+const checkAuth = require("../middleware/check-auth");
 
 const regAdminChecker = [
-	check('NIK').trim().notEmpty(),
-	check('firstName').trim().notEmpty(),
-	check('lastName').trim().notEmpty(),
-	check('email').normalizeEmail().isEmail(),
-	check('gender').trim().notEmpty(),
-	check('dateOfBirth').trim().notEmpty(),
-	check('address').trim().notEmpty(),
-	check('phoneNumber').trim().notEmpty(),
-	check('jobTitle').trim().notEmpty(),
-	check('verificationKey').trim().notEmpty()
+  // check("NIK").trim().notEmpty(),
+  // check("firstName").trim().notEmpty(),
+  // check("lastName").trim().notEmpty(),
+  // check("email").normalizeEmail().isEmail(),
+  // check("gender").trim().notEmpty(),
+  // check("dateOfBirth").trim().notEmpty(),
+  // check("address").trim().notEmpty(),
+  // check("phoneNumber").trim().notEmpty(),
+  // check("jobTitle").trim().notEmpty(),
+  // check("verificationKey").trim().notEmpty(),
 ];
 
 // router.get('/admlog', adminControllers.admlog);
 
-router.post('/admreg', regAdminChecker, adminControllers.admReg);
-router.post('/admsign', adminControllers.admSign);
+router.post("/admreg", regAdminChecker, adminControllers.admReg);
+router.post("/admsign", adminControllers.admSign);
+router.get("/order/reguler", adminControllers.getOrderReguler);
+router.post("/approve/reg", adminControllers.approveOrderReg);
 router.use(checkAuth);
-router.get('/jobs', adminControllers.getWholeJobs);
-router.get('/applicants', adminControllers.getWholeApplicants);
-router.get('/companies', adminControllers.getWholeCompanies);
-router.post('/order/reg', adminControllers.createOrderReg);
-router.post('/approve/reg', adminControllers.approveOrderReg);
-router.post('/order/bc', adminControllers.createOrderBC);
-router.post('/approve/bc', adminControllers.approveOrderBC);
-router.get('/:jobid/applicants', adminControllers.getApplicantsFromJob);
-router.get('/:applicantid/jobs', adminControllers.getJobsFromApplicant);
-router.post('/:companyid/activate', adminControllers.activateCompany);
-router.post('/:companyid/block', adminControllers.blockCompany);
+router.get("/:companyid/order/reg", adminControllers.getCompanyOrder);
+router.get("/order/:orderid/invoice", adminControllers.getOrderInvoice);
+router.get("/jobs", adminControllers.getWholeJobs);
+router.get("/applicants", adminControllers.getWholeApplicants);
+router.post("/order/reg", adminControllers.createOrderReg);
+router.post("/order/bc", adminControllers.createOrderBC);
+router.post("/approve/bc", adminControllers.approveOrderBC);
+router.get("/companies", adminControllers.getWholeCompanies);
+router.get("/:jobid/applicants", adminControllers.getApplicantsFromJob);
+router.get("/:applicantid/jobs", adminControllers.getJobsFromApplicant);
+router.post("/:companyid/activate", adminControllers.activateCompany);
+router.post("/:companyid/block", adminControllers.blockCompany);
 
-router.delete('/feedback', adminControllers.deleteFeed);
+router.delete("/feedback", adminControllers.deleteFeed);
 
 module.exports = router;
