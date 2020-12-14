f96054002e6df8bf888014ca938340a126869274
Tommy Pranoto <tommyprn8@gmail.com>
2020-12-14 10:29:27 +0700

    debug push kemarin

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const { check } = require('express-validator');
-const adminControllers = require('../controllers/admin-controllers');
+const express = require("express");
+const { check } = require("express-validator");
+const adminControllers = require("../controllers/admin-controllers");
 
 const router = express.Router();
-const checkAuth = require('../middleware/check-auth');
+const checkAuth = require("../middleware/check-auth");
 
 const regAdminChecker = [
-
   check("NIK").trim().notEmpty(),
   check("firstName").trim().notEmpty(),
   check("lastName").trim().notEmpty(),
@@ -17,41 +16,45 @@ const regAdminChecker = [
   check("phoneNumber").trim().notEmpty(),
   check("jobTitle").trim().notEmpty(),
   check("verificationKey").trim().notEmpty(),
-
 ];
 
 // router.get('/admlog', adminControllers.admlog);
 
-
-router.post('/admreg', regAdminChecker, adminControllers.admReg);
-router.post('/admsign', adminControllers.admSign);
+router.post("/admreg", regAdminChecker, adminControllers.admReg);
+router.post("/admsign", adminControllers.admSign);
 router.use(checkAuth);
 
-router.post('/order/reg', adminControllers.createOrderReg);
+router.post("/order/es", adminControllers.createOrderES);
+router.get("/order/es", adminControllers.getWholeOrderES);
+router.get("/:companyid/order/es", adminControllers.getCompanyOrderES);
+router.get("/order/es/:orderid", adminControllers.getOneOrderES);
+router.post("/order/es/addcandidate", adminControllers.addCandidateES);
+router.post(
+  "/order/es/updatecandidate",
+  adminControllers.updateCandidateStatusES
+);
+router.post("/order/es/updateorder", adminControllers.updateOrderStatusES);
 
-router.get('/order/es', adminControllers.getWholeOrderES);
-router.get('/order/es/:orderid', adminControllers.getOneOrderES);
-router.post('/order/es', adminControllers.createOrderES);
-router.post('/order/es/addcandidate', adminControllers.addCandidateES);
-router.post('/order/es/updatecandidate', adminControllers.updateCandidateStatusES);
-router.post('/order/es/updateorder', adminControllers.updateOrderStatusES);
+router.post("/order/bc", adminControllers.createOrderBC);
+router.get("/order/bc", adminControllers.getOrderBC);
+router.post("/approve/bc", adminControllers.approveOrderBC);
+router.get("/:companyid/order/bc", adminControllers.getCompanyOrderBC);
 
-router.get('/order/reguler', adminControllers.getOrderReguler);
-router.get('/order/:orderid/invoice', adminControllers.getOrderInvoice);
-router.post('/approve/reg', adminControllers.approveOrderReg);
+router.post("/order/reg", adminControllers.createOrderReg);
+router.get("/order/reguler", adminControllers.getOrderReguler);
+router.post("/approve/reg", adminControllers.approveOrderReg);
+router.get("/:companyid/order/reg", adminControllers.getCompanyOrder);
 
-router.get('/jobs', adminControllers.getWholeJobs);
-router.get('/applicants', adminControllers.getWholeApplicants);
-router.post('/order/bc', adminControllers.createOrderBC);
-router.post('/approve/bc', adminControllers.approveOrderBC);
-router.get('/companies', adminControllers.getWholeCompanies);
-router.get('/:companyid/order/reg', adminControllers.getCompanyOrder);
-router.get('/:jobid/applicants', adminControllers.getApplicantsFromJob);
-router.get('/:applicantid/jobs', adminControllers.getJobsFromApplicant);
-router.post('/:companyid/activate', adminControllers.activateCompany);
-router.post('/:companyid/block', adminControllers.blockCompany);
+router.get("/order/:orderid/invoice", adminControllers.getOrderInvoice);
 
-router.delete('/feedback', adminControllers.deleteFeed);
+router.get("/jobs", adminControllers.getWholeJobs);
+router.get("/applicants", adminControllers.getWholeApplicants);
+router.get("/companies", adminControllers.getWholeCompanies);
+router.get("/:jobid/applicants", adminControllers.getApplicantsFromJob);
+router.get("/:applicantid/jobs", adminControllers.getJobsFromApplicant);
+router.post("/:companyid/activate", adminControllers.activateCompany);
+router.post("/:companyid/block", adminControllers.blockCompany);
 
+router.delete("/feedback", adminControllers.deleteFeed);
 
 module.exports = router;
