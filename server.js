02eecaa9d910f0aaf14672e2a1a5ed8761186ecb
novrandobilly <novrandobilly@gmail.com>
2021-03-12 10:02:38 +0700

    add cors package

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const schedule = require('node-schedule');
 
 const jobsRoutes = require('./routes/jobs-routes');
@@ -19,12 +20,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-	next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// 	next();
+// });
 
 app.use('/api/jobs', jobsRoutes);
 app.use('/api/users', usersRoutes);
