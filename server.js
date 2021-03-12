ee43e6815763a03bc49ea4c3ba27ad6909281d29
novrandobilly <novrandobilly@gmail.com>
2021-03-12 10:46:34 +0700

    remove application/json from ACAO

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 // app.use(cors());
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*, application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 	next();
