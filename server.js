4f32ea4cd47a14c19d6b058269df0ca20e82f4d5
novrandobilly <novrandobilly@gmail.com>
2021-03-12 12:22:30 +0700

    refine

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ const app = express();
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	// res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 	next();
 });
 app.use(bodyParser.urlencoded({ extended: false }));
