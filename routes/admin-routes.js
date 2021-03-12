6f93744b0a04b96b57858619bf6d054b09228480
novrandobilly <novrandobilly@gmail.com>
2021-03-12 12:51:33 +0700

    refine

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 const adminControllers = require('../controllers/admin-controllers');
 const { adminAvatar } = require('../middleware/file-upload');
+const cors = require('cors');
 
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
@@ -23,7 +24,7 @@ const regAdminChecker = [
 
 router.post('/admreg', regAdminChecker, adminControllers.admReg);
 router.post('/admsign', adminControllers.admSign);
-// router.use(checkAuth);
+router.use(checkAuth);
 
 router.patch('/:adminid/profile', adminAvatar, adminControllers.updateAdminProfile);
 router.get('/:adminid/profile', adminControllers.getAdminDetails);
@@ -43,7 +44,7 @@ router.post('/order/es/updatecandidate', adminControllers.updateCandidateStatusE
 router.post('/order/es/updateorder', adminControllers.updateOrderStatusES);
 router.delete('/order/es/deletecandidate', adminControllers.deleteCandidateES);
 
-router.post('/order/reg', adminControllers.createOrderReg);
+router.post('/order/reg', cors(), adminControllers.createOrderReg);
 router.get('/order/reg', adminControllers.getWholeOrderREG);
 router.post('/approve/reg', adminControllers.approveOrderReg);
 router.get('/:companyid/order/reg', adminControllers.getCompanyOrder);
