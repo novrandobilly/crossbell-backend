fa37b61806077294b9d9ae82046344b63658ee00
novrandobilly <novrandobilly@gmail.com>
2021-03-12 13:45:25 +0700

    refine

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
