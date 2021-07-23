137f606d94fdf90182c2ca7f606f9f69b36fe3fb
novrandobilly <novrandobilly@gmail.com>
2021-07-23 10:01:27 +0700

    remove authentication on get one job

--- routes/jobs-routes.js
+++ routes/jobs-routes.js
@@ -19,6 +19,7 @@ const updateJobChecker = [check('jobDescriptions').trim().isLength({ min: 5 })];
 // =======================ROUTES=======================
 router.get('/', jobsControllers.getAllAvailableJobs);
 router.get('/:companyid/all/jobs', jobsControllers.getJobsInCompany);
+router.get('/:jobid', jobsControllers.getSpecificJob);
 
 router.use(checkAuth);
 
@@ -28,7 +29,6 @@ router.patch('/draft/:jobid', jobsControllers.editJobDraft);
 router.post('/draft', jobsControllers.saveJobDraft);
 router.patch('/:jobid/release', createJobChecker, jobsControllers.releaseJob);
 router.patch('/:jobid', updateJobChecker, jobsControllers.updateJob);
-router.get('/:jobid', jobsControllers.getSpecificJob);
 
 router.delete('/:jobid', jobsControllers.deleteJob);
 
