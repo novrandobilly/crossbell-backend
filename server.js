66a891956200132b3de9e3d5ac4d5f939d76123d
novrandobilly <novrandobilly@gmail.com>
2021-08-02 11:58:27 +0700

    Hanya mengganti server

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token'
-  );
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 });
@@ -54,8 +51,8 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    // `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@crossbelldb-shard-00-00.fiwox.mongodb.net:27017,crossbelldb-shard-00-01.fiwox.mongodb.net:27017,crossbelldb-shard-00-02.fiwox.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-13v3tm-shard-0&authSource=admin&retryWrites=true&w=majority`,
-    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.1ncnh.mongodb.net:27017,cluster0-shard-00-01.1ncnh.mongodb.net:27017,cluster0-shard-00-02.1ncnh.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-99le7k-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@crossbelldb-shard-00-00.fiwox.mongodb.net:27017,crossbelldb-shard-00-01.fiwox.mongodb.net:27017,crossbelldb-shard-00-02.fiwox.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-13v3tm-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    // `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.1ncnh.mongodb.net:27017,cluster0-shard-00-01.1ncnh.mongodb.net:27017,cluster0-shard-00-02.1ncnh.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-99le7k-shard-0&authSource=admin&retryWrites=true&w=majority`,
     // 'mongodb://localhost/crossbelldev',
     {
       useNewUrlParser: true,
@@ -68,6 +65,6 @@ mongoose
     app.listen(process.env.PORT || 5000);
     console.log('Server is listening. Connected to the database');
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
